3d2028571c21b8ac67166c5b2d32618a6ac46aa0
Vigneshwaran Ravichandran <vgnshlvnz@users.noreply.github.com>
2021-08-03 22:02:31 +0800

    Create dns_resolve.sh
    Complete script done without git

---
+++ dns_resolve.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+domain="<INSERT_YOUR_FQDB>"
+aname_resolve=$(/usr/bin/host $domain| awk 'NR==2{print $5}')
+dns_refused="5(REFUSED)"
+resolve_success=$(/usr/bin/touch /root/$domain)
+
+while [ $aname_resolve = $dns_refused  ]
+do
+        echo 'fail'
+        exit 1
+        if ( $? -eq 0 )
+        then
+                $resolve_success
+        fi
+done
