a044c6e51c923d0a4e82ccaaaa72b2bd41f7ae41
Vigneshwaran Ravichandran <vgnshlvnz@users.noreply.github.com>
2021-08-03 23:34:20 +0800

    updated dns_resolve.sh
    updated the logic. Should work on non-existent domain.

--- dns_resolve.sh
+++ dns_resolve.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-domain="<INSERT_YOUR_FQDB>"
+domain="ameldawrites.com"
 aname_resolve=$(/usr/bin/host $domain| awk 'NR==2{print $5}')
 dns_refused="5(REFUSED)"
 resolve_success=$(/usr/bin/touch /root/$domain)
 
-while [ $aname_resolve = $dns_refused  ]
+while  [ "$aname_resolve" = $dns_refused  ]
 do
         echo 'fail'
         exit 1
-        if ( $? -eq 0 )
-        then
+         if  [ $? -eq 0 ]
+         then
                 $resolve_success
-        fi
+         fi
+        echo 'script failed'
 done
